a24c7e040956ca7354cf7844de682127b191ebb5
Miguel Méndez Murias <miguelmendeztech@gmail.com>
2024-02-02 10:42:31 +0100

    SDI-2324-310-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi2324310spring/Sdi2324310SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2324310spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2324310SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2324310SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniovi/sdi2324310spring/Sdi2324310SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2324310spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2324310SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
