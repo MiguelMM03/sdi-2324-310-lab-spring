4895402e327f63318806569795b2f596c3841428
Miguel Méndez Murias <miguelmendeztech@gmail.com>
2024-02-09 20:07:12 +0100

    SDI-2324-310-3.3-Acceso a datos con entidades relacionadas.

--- src/main/java/com/uniovi/sdi2324310spring/controllers/MarksController.java
+++ src/main/java/com/uniovi/sdi2324310spring/controllers/MarksController.java
@@ -2,6 +2,7 @@ package com.uniovi.sdi2324310spring.controllers;
 
 import com.uniovi.sdi2324310spring.entities.Mark;
 import com.uniovi.sdi2324310spring.services.MarksService;
+import com.uniovi.sdi2324310spring.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,9 +10,13 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MarksController {
-    @Autowired //Inyectar el servicio
-    private MarksService marksService;
-
+    // Inyectamos el servicio por inyección basada en constructor
+    private final MarksService marksService;
+    private final UsersService usersService;
+    public MarksController(MarksService marksService, UsersService usersService) {
+        this.marksService = marksService;
+        this.usersService = usersService;
+    }
     @RequestMapping("/mark/list")
     public String getList(Model model) {
         model.addAttribute("markList", marksService.getMarks());
@@ -28,7 +33,8 @@ public class MarksController {
         return "redirect:/mark/list";
     }
     @RequestMapping(value = "/mark/add")
-    public String getMark() {
+    public String getMark(Model model) {
+        model.addAttribute("usersList", usersService.getUsers());
         return "mark/add";
     }
     @RequestMapping("/mark/details/{id}")
@@ -45,12 +51,16 @@ public class MarksController {
     @RequestMapping(value = "/mark/edit/{id}")
     public String getEdit(Model model, @PathVariable Long id) {
         model.addAttribute("mark", marksService.getMark(id));
+        model.addAttribute("usersList", usersService.getUsers());
         return "mark/edit";
     }
     @RequestMapping(value="/mark/edit/{id}", method=RequestMethod.POST)
     public String setEdit(@ModelAttribute Mark mark, @PathVariable Long id){
-        mark.setId(id);
-        marksService.addMark(mark);
+        Mark originalMark = marksService.getMark(id);
+        // modificar solo score y description
+        originalMark.setScore(mark.getScore());
+        originalMark.setDescription(mark.getDescription());
+        marksService.addMark(originalMark);
         return "redirect:/mark/details/"+id;
     }
 }
---
+++ src/main/java/com/uniovi/sdi2324310spring/repositories/UsersRepository.java
@@ -0,0 +1,6 @@
+package com.uniovi.sdi2324310spring.repositories;
+
+import com.uniovi.sdi2324310spring.entities.User;
+import org.springframework.data.repository.CrudRepository;
+public interface UsersRepository extends CrudRepository<User, Long>{
+}
