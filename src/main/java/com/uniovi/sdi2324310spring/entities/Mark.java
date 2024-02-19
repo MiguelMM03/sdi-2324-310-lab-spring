302b26834efeddee729e0a660e37d7e0db216b7c
Miguel Méndez Murias <miguelmendeztech@gmail.com>
2024-02-19 21:15:19 +0100

    SDI-2324-310-4.3-SpringBoot-Control de acceso por roles.

--- src/main/java/com/uniovi/sdi2324310spring/entities/Mark.java
+++ src/main/java/com/uniovi/sdi2324310spring/entities/Mark.java
@@ -10,6 +10,7 @@ public class Mark {
     private Long id;
     private String description;
     private Double score;
+    private Boolean resend=false;
 
     @ManyToOne
     @JoinColumn(name="user_id")
@@ -69,5 +70,11 @@ public class Mark {
         if (o == null || getClass() != o.getClass()) return false;
         Mark mark = (Mark) o;
         return Objects.equals(id, mark.id);
+    }
+    public Boolean getResend() {
+        return resend;
+    }
+    public void setResend(Boolean resend) {
+        this.resend = resend;
     }
 }
